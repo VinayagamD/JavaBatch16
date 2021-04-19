02e2a4c4674e5278d24d31f7318dbe3bff692a3e
VinayagamD <vinayagam.d.ganesh@gmail.com>
2021-04-19 09:03:15 +0530

    First Code

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello to Java Training");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
